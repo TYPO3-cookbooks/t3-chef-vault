189778abaa3263002924bad2c5ddf68527e88b11
Steffen Gebert <steffen.gebert@typo3.org>
2017-10-03 21:23:08 +0200

    [TESTS] Update test harness

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "A library cookbook extending the chef-vault library cookbook"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
-version          "1.0.3"
 depends          "chef-vault", "~> 1.3.3"
